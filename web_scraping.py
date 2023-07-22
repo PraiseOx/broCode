c908eb701fcd15f27ce3e089f9333aedc0efc4d2
Praise Onyehanere <31757537+PraizOnyee@users.noreply.github.com>
2023-07-22 20:51:52 +0100

    First trial at Web scraping with Youtube Tutorial from Tinkernut

---
+++ web_scraping.py
@@ -0,0 +1,37 @@
+import csv
+
+from bs4 import BeautifulSoup
+import requests
+
+page_to_scrape = requests.get("http://quotes.toscrape.com")
+
+soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
+
+quotes = soup.findAll('span', attrs={'class':'text'})
+
+authors = soup.findAll('small', attrs={'class':'author'})
+
+"""for quote in quotes:
+    print(quote.text)
+
+for author in authors:
+    print(author.text)"""
+
+"""with open('scraped_quotes.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+
+    writer.writerow(["S/N", "QUOTES", "AUTHORS"])
+    for i, (quote, author) in enumerate(zip(quotes, authors), start=1):
+        print(f"{quote.text} - {author.text}")
+        writer.writerow([i, quote.text, author.text])
+
+print("Quotes saved to scraped_quotes.csv.")"""
+
+file = open("scraped_quotes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
+for quote, author in zip(quotes, authors):
+     print(quote.text + "_" + author.text)
+     writer.writerow([quote.text, author.text])
+file.close
